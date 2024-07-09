788457cc93c52d63d07cc49eca3e3cfa1f475f19
notjuliet <notjuliet@riseup.net>
2024-07-09 16:12:43 +0200

    added neopronouns

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,13 @@ export const PRONOUNS: Record<string, string> = {
   ask: "ask",
   avoid: "avoid",
   "look at bio": "bio",
+  "fae/faer": "fae",
+  "bun/buns": "bun",
+  "xe/xir": "xe",
+  "drae/draer": "drae",
+  "e/em/eir": "e",
+  "sier/siehn/siere": "sier",
+  "ey/em/eir": "ey",
 };
 
 export const URIs: Record<string, string> = {
@@ -30,6 +37,20 @@ export const URIs: Record<string, string> = {
     "ask",
   "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwss4vc4cw2x":
     "any/all",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu55hxj4b2a":
+    "fae/faer",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5d6rq352v":
+    "bun/buns",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5s4lt6f2k":
+    "xe/xir",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5sjyg4m2f":
+    "drae/draer",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5stootb2q":
+    "e/em/eir",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu5taglmr2q":
+    "sier/siehn/siere",
+  "at://did:plc:wkoofae5uytcm7bjncmev6n6/app.bsky.feed.post/3kwu6b3cnd52k":
+    "ey/em/eir",
 };
 
 export const DID = process.env.DID ?? "";
