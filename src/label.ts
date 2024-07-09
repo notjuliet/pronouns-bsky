609774b02c9f323d48ff718a953dfab60ef970eb
Ollie Hsieh <ollie@cloudflare.com>
2024-07-09 12:49:38 -0700

    fix: Use one way to get subject DID

--- src/label.ts
+++ src/label.ts
@@ -1,75 +1,86 @@
 import { AppBskyActorDefs, BskyAgent } from "@atproto/api";
 import { DID, PRONOUNS, URIs } from "./constants.js";
 
+const CREATE_LABEL_VALS: string[] = [];
+const NEGATE_LABEL_VALS: string[] = [];
+const SUBJECT_DID = "did:plc:5zbvredb4ovf3wrcslt56czc";
+
 export const label = async (
   agent: BskyAgent,
   subject: string | AppBskyActorDefs.ProfileView,
-  uri: string,
+  uri: string
 ) => {
-  const did = AppBskyActorDefs.isProfileView(subject) ? subject.did : subject;
-  const repo = await agent
-    .withProxy("atproto_labeler", DID)
-    .api.tools.ozone.moderation.getRepo({ did: did })
-    .catch((err) => {
+  // const did = AppBskyActorDefs.isProfileView(subject) ? subject.did : subject;
+  // console.log(did);
+  console.log("Trying to fetch repo ...");
+
+  try {
+    const repo = await agent
+      .withProxy("atproto_labeler", DID)
+      .api.tools.ozone.moderation.getRepo({
+        did: SUBJECT_DID,
+      });
+
+    // const post = URIs[uri];
+
+    // if (repo.data.labels && (post ?? "").includes("Like this post to delete")) {
+    //   try {
+    //     await agent
+    //       .withProxy("atproto_labeler", DID)
+    //       .api.tools.ozone.moderation.emitEvent({
+    //         event: {
+    //           $type: "tools.ozone.moderation.defs#modEventLabel",
+    //           createLabelVals: [],
+    //           negateLabelVals: repo.data.labels.map((label) => label.val),
+    //         },
+    //         subject: {
+    //           $type: "com.atproto.admin.defs#repoRef",
+    //           did: did,
+    //         },
+    //         createdBy: agent.session!.did,
+    //         createdAt: new Date().toISOString(),
+    //         subjectBlobCids: [],
+    //       });
+
+    //     console.log(`Deleted labels for ${did}`);
+    //   } catch (err) {
+    //     console.log({
+    //       error: err,
+    //     });
+    //   }
+    //   return;
+    // }
+
+    // if (repo.data.labels && repo.data.labels.length >= 4) return;
+
+    // if (PRONOUNS[post]) {
+    try {
+      await agent
+        .withProxy("atproto_labeler", DID)
+        .api.tools.ozone.moderation.emitEvent({
+          event: {
+            $type: "tools.ozone.moderation.defs#modEventLabel",
+            createLabelVals: CREATE_LABEL_VALS,
+            negateLabelVals: NEGATE_LABEL_VALS,
+          },
+          subject: {
+            $type: "com.atproto.admin.defs#repoRef",
+            did: repo.data.did,
+          },
+          createdBy: agent.session!.did,
+          createdAt: new Date().toISOString(),
+          subjectBlobCids: [],
+        });
+      console.log(`Labeled ${repo.data.did} with ask`);
+    } catch (err) {
       console.log({
         error: err,
       });
-    });
-
-  if (!repo) return;
-
-  const post = URIs[uri];
-
-  if (repo.data.labels && (post ?? "").includes("Like this post to delete")) {
-    await agent
-      .withProxy("atproto_labeler", DID)
-      .api.tools.ozone.moderation.emitEvent({
-        event: {
-          $type: "tools.ozone.moderation.defs#modEventLabel",
-          createLabelVals: [],
-          negateLabelVals: repo.data.labels.map((label) => label.val),
-        },
-        subject: {
-          $type: "com.atproto.admin.defs#repoRef",
-          did: did,
-        },
-        createdBy: agent.session!.did,
-        createdAt: new Date().toISOString(),
-        subjectBlobCids: [],
-      })
-      .catch((err) => {
-        console.log({
-          error: err,
-        });
-      })
-      .then(() => console.log(`Deleted labels for ${did}`));
-    return;
+    }
+    // }
+  } catch (err) {
+    console.log(err);
   }
 
-  if (repo.data.labels && repo.data.labels.length >= 4) return;
-
-  if (PRONOUNS[post]) {
-    await agent
-      .withProxy("atproto_labeler", DID)
-      .api.tools.ozone.moderation.emitEvent({
-        event: {
-          $type: "tools.ozone.moderation.defs#modEventLabel",
-          createLabelVals: [PRONOUNS[post]],
-          negateLabelVals: [],
-        },
-        subject: {
-          $type: "com.atproto.admin.defs#repoRef",
-          did: did,
-        },
-        createdBy: agent.session!.did,
-        createdAt: new Date().toISOString(),
-        subjectBlobCids: [],
-      })
-      .catch((err) => {
-        console.log({
-          error: err,
-        });
-      })
-      .then(() => console.log(`Labeled ${did} with ${post}`));
-  }
+  console.log("Finished run ...");
 };
--- src/main.ts
+++ src/main.ts
@@ -7,34 +7,39 @@ import "dotenv/config";
 
 const subscribe = async () => {
   const agent = await getAgent();
+
+  console.log(`Logged in as: ${agent.session?.handle}`);
+
   let cursorSave = 0;
 
   // TODO: save cursor to a file
-  const firehose = new Firehose({ cursor: process.env.CURSOR ?? "" });
+  const firehose = new Firehose({ cursor: "762474730" });
 
   firehose.on("error", ({ cursor }) => {
     console.log(`Firehose errored on cursor: ${cursor}`);
   });
 
-  firehose.on("open", () => {
-    setInterval(() => {
-      console.log(`cursor: ${cursorSave}`);
-    }, 60000);
-  });
+  let count = 0;
 
-  firehose.on("commit", async (commit) => {
-    cursorSave = commit.seq;
-    commit.ops.forEach(async (op) => {
-      if (op.action !== "delete" && AppBskyFeedLike.isRecord(op.record)) {
-        if ((op.record.subject.uri ?? "").includes(DID)) {
-          if ((op.record.subject.uri ?? "").includes("app.bsky.feed.post")) {
-            await label(agent, commit.repo, op.record.subject.uri).catch(
-              (err) => console.error(err),
-            );
-          }
+  firehose.on("commit", (commit) => {
+    // cursorSave = commit.seq;
+
+    if (count === 0) {
+      console.log("Trying to run ...");
+      commit.ops.forEach(async (op) => {
+        if (op.action !== "delete" && AppBskyFeedLike.isRecord(op.record)) {
+          // if ((op.record.subject.uri ?? "").includes(DID)) {
+          //   if ((op.record.subject.uri ?? "").includes("app.bsky.feed.post")) {
+          label(agent, commit.repo, op.record.subject.uri).catch((err) =>
+            console.error(err)
+          );
+          //   }
+          // }
         }
-      }
-    });
+      });
+    }
+
+    count++;
   });
 
   firehose.start();
