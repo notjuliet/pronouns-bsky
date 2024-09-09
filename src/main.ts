092b9a1784753f802b90b03cf334a7f3a1eee829
notjuliet <notjuliet@riseup.net>
2024-09-09 19:50:32 +0200

    refactor main

--- src/main.ts
+++ src/main.ts
@@ -8,29 +8,28 @@ const subscribe = async () => {
   let cursor = 0;
   let intervalID: NodeJS.Timeout;
   const cursorFile = fs.readFileSync("cursor.txt", "utf8");
-
   const relay = cursorFile ? RELAY.concat("&cursor=", cursorFile) : RELAY;
-  if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
   const ws = new WebSocket(relay);
+  if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
 
   ws.on("error", (err) => console.error(err));
 
   ws.on("open", () => {
     intervalID = setInterval(() => {
       console.log(`${new Date().toISOString()}: ${cursor}`);
-      fs.writeFile("cursor.txt", cursor.toString(), (err) => console.log(err));
+      fs.writeFile("cursor.txt", cursor.toString(), (err) => {
+        if (err) console.log(err);
+      });
     }, 60000);
   });
 
-  ws.on("close", () => {
-    clearInterval(intervalID);
-  });
+  ws.on("close", () => clearInterval(intervalID));
 
   ws.on("message", (data) => {
     const event: EventStream = JSON.parse(data.toString());
     cursor = event.time_us;
     if (
-      event.commit?.record?.subject?.uri?.includes(DID + "/app.bsky.feed.post")
+      event.commit?.record?.subject?.uri?.includes(`${DID}/app.bsky.feed.post`)
     ) {
       label(event.did, event.commit.record.subject.uri.split("/").pop()!);
     }
