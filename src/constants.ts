63e2376c6903e02ddca81d399ba42a7ca4ce8cf5
notjuliet <notjuliet@riseup.net>
2024-09-21 20:13:53 +0200

    add endpoint ENV / remove cursor retrieval

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ import "dotenv/config";
 
 export const DID = process.env.DID ?? "";
 export const SIGNING_KEY = process.env.SIGNING_KEY ?? "";
-export const PORT = process.env.PORT ?? 4002;
+export const PORT = 4002;
+export const URL = process.env.URL ?? "wss://jetstream.atproto.tools/subscribe";
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,13 @@
 import { label_video } from "./label.js";
-import fs from "node:fs";
+import { URL } from "./constants.js";
 import { Jetstream } from "@skyware/jetstream";
 
-let intervalID: NodeJS.Timeout;
-const cursorFile = fs.readFileSync("cursor.txt", "utf8");
-if (cursorFile) console.log(`Initiate firehose at cursor ${cursorFile}`);
-
 const jetstream = new Jetstream({
+  endpoint: URL,
   wantedCollections: ["app.bsky.feed.post"],
-  cursor: cursorFile ?? 0,
-});
-
-jetstream.on("open", () => {
-  intervalID = setInterval(() => {
-    if (jetstream.cursor) {
-      console.log(`${new Date().toISOString()}: ${jetstream.cursor}`);
-      fs.writeFile("cursor.txt", jetstream.cursor.toString(), (err) => {
-        if (err) console.log(err);
-      });
-    }
-  }, 60000);
 });
 
 jetstream.on("error", (err) => console.error(err));
-
-jetstream.on("close", () => clearInterval(intervalID));
 
 jetstream.onCreate("app.bsky.feed.post", (event) => {
   if (
